5b59826ccb646e5d5f01074df224c9fb080bb259
Elias Lajimi <elajimi@mi-c2-p8.s19.be>
2020-10-08 21:07:57 +0200

    ft_list_size.s

--- ft_list_sort.s
+++ ft_list_sort.s
@@ -1,14 +1,20 @@
 global _ft_list_sort
 
 _ft_list_sort:
-	mov		rdx, -8	;pointer iterator
+	push	r13
+	push	r14
+	mov		rdx, - 8				;pointer iterator
 _inc:
-	add		rdx, 8
-_swap:
-	mov		r13, QWORD [rdi] ;struct
+	add		rdx, 8 
+_iter:
+	mov		r13, QWORD [rdi + rdx]	;moving struct to register 
 	cmp		r13, 0
-	je		_order
-	mov		r14, QWORD [rdi+rdx];from pointer to pointer
+	je		_order					;call to order function
+	mov		r14, QWORD [rdi - 8]	;load void data in r14
+	
+
+
+_order:
 
 	pop		r13
 	pop		r14
